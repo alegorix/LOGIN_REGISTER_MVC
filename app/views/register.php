40d5d019f3f12d765f3a8a685348bf9d2a405cd0
alegorix <51210510+alegorix@users.noreply.github.com>
2025-03-17 15:38:56 +0100

    Create register.php

---
+++ app/views/register.php
@@ -0,0 +1,5 @@
+<form method="post" action="index.php?action=register">
+    <input type="text" name="username" required placeholder="Nom d'utilisateur">
+    <input type="password" name="password" required placeholder="Mot de passe">
+    <button type="submit">S'inscrire</button>
+</form>
