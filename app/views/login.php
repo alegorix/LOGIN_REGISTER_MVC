f73256bf7b4b4acb9e4ff37786dbf78b613c5a46
alegorix <51210510+alegorix@users.noreply.github.com>
2025-03-17 15:39:35 +0100

    Create login.php

---
+++ app/views/login.php
@@ -0,0 +1,5 @@
+<form method="post" action="index.php?action=login">
+    <input type="text" name="username" required placeholder="Nom d'utilisateur">
+    <input type="password" name="password" required placeholder="Mot de passe">
+    <button type="submit">Se connecter</button>
+</form>
