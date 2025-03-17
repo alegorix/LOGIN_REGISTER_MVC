30ffa8f31829ce7d468e718d9fd359b37ca4cac1
alegorix <51210510+alegorix@users.noreply.github.com>
2025-03-17 15:36:23 +0100

    Create config.php

---
+++ app/config.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+define('BASE_URL', 'http://localhost/mvc-login/');
+require_once 'Database.php';
+require_once 'models/User.php';
+require_once 'controllers/AuthController.php';
+?>
